797ec4b8b07b6d48c4358b74151ae04cf80d27ff
Pat Lehay <pat@patleahy.com>
2019-11-19 07:58:45 -0800

    Add examples.

--- that1song/examples.py
+++ that1song/examples.py
@@ -2,31 +2,36 @@ searches = [
     { 
         'name': 'Smells Like Teen Spirit', 
         'img': 'https://i.scdn.co/image/dd7e7cf73d58ea713051363e308a343e46bbcf7f',
-        'message': 'Check out the<br /><a href="https://open.spotify.com/album/2GKKvM2ex7DfwJpTcOSTn4">Scala&nbsp;&amp;&nbsp;Kolacny&nbsp;Brothers</a> cover.'
-    },
-    {
-        'name': 'Nightswimming',
-        'img': 'https://i.scdn.co/image/b161180e4bd65c27f65625e0a9f57d7352eefdb9',
-        'message': ''
+        'msg': 'Check out the<br /><a href="https://open.spotify.com/album/2GKKvM2ex7DfwJpTcOSTn4">Scala&nbsp;&amp;&nbsp;Kolacny&nbsp;Brothers</a> cover.'
     },
     {
         'name': '99 Red Balloons',
         'img': 'https://i.scdn.co/image/ab67616d0000485156426f82825bdd491188b7f1',
-        'message': 'Unfortunately, this won&#39;t find <a href="https://open.spotify.com/album/78hVLZZJhaXgrnfXKc6yxF">99&nbsp;Luftballons</a></i>.</p>'
-    },
-    {
-        'name': 'Buffalo Stance',
-        'img': 'https://i.scdn.co/image/ab67616d0000485173409947013bf11fedefaef2',
-        'message': ''
+        'msg': 'Unfortunately, this won&#39;t find <a href="https://open.spotify.com/album/78hVLZZJhaXgrnfXKc6yxF">99&nbsp;Luftballons</a></i>.</p>'
     },
     {
         'name': 'Hurt',
         'img': 'https://i.scdn.co/image/7b77fb435a5770985ad34493196a99989a1bd1cb',
-        'message': '<a href="https://open.spotify.com/album/3nJnyDV8fwFpffo0EyHQto">Trent&nbsp;Reznor</a> or <a href="https://open.spotify.com/album/2BlL4Gv2DLPu8p58Wcmlm9">Johnny Cash</a>'
+        'msg': '<a href="https://open.spotify.com/album/3nJnyDV8fwFpffo0EyHQto">Trent&nbsp;Reznor</a> &amp; <a href="https://open.spotify.com/album/2BlL4Gv2DLPu8p58Wcmlm9">Johnny Cash</a>'
     },
     {
         'name': 'All Along the Watchtower',
         'img': 'https://i.scdn.co/image/ab67616d0000485181efb7dfa40bbfa10a199e38',
-        'message': '<a href="https://open.spotify.com/album/2KzCDxKpgLqBffHu1IZ7Kn">Dylan</a> or <a href="https://open.spotify.com/album/5z090LQztiqh13wYspQvKQ">Hendrix</a>'
+        'msg': '<a href="https://open.spotify.com/album/2KzCDxKpgLqBffHu1IZ7Kn">Dylan</a> &amp; <a href="https://open.spotify.com/album/5z090LQztiqh13wYspQvKQ">Hendrix</a>'
+    },
+    {
+        'name': 'Nothing Compares 2 U',
+        'img': 'https://i.scdn.co/image/61987ff69716577c4ffe26ab7b3106c6bb2d7bd3',
+        'msg': '<a href="https://open.spotify.com/album/1X1So5oX4TCFGdK6BN9UXs">Prince</a> &amp; <a href="https://open.spotify.com/album/34hQFIwGTLf03BZQmGL0iy">Sin&eacute;ad O&#39;Connor</a>'
+    },
+    {
+        'name': "Knockin' On Heaven's Door",
+        'img': 'https://i.scdn.co/image/ab67616d000048516c86683d20c72e3874c11c6d',
+        'msg': '<a href="https://open.spotify.com/album/2Pj2kZM5XpyIeyFBTAVulL">Dylan</a> &amp; <a href="https://open.spotify.com/album/00eiw4KOJZ7eC3NBEpmH4C">Guns N&#39; Roses</a>'
+    },
+    {
+        'name': 'Take On Me',
+        'img': 'https://i.scdn.co/image/ab67616d00004851e8dd4db47e7177c63b0b7d53',
+        'msg': '<a href="https://open.spotify.com/album/1ER3B6zev5JEAaqhnyyfbf">a-ha</a> &amp; <a href="https://open.spotify.com/album/65sHj9PvsbyD0uugGHjueN">Weezer</a>,<br/>but not <a href="https://www.youtube.com/watch?v=-xKM3mGt2pE">a-ha 2017 unplugged</a> version.'
     }
 ]
