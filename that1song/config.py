9e240fc29eb91a6ac28a5af9f0bdb71cd114abac
Pat Lehay <pat@patleahy.com>
2019-11-19 21:54:46 -0800

    Config to work in Azure.

--- that1song/config.py
+++ that1song/config.py
@@ -6,7 +6,7 @@ SPOTIFY_CLIENT_ID       = environ.get("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET   = environ.get("SPOTIFY_CLIENT_SECRET")
 
 SESSION_TYPE = 'filesystem' # TODO Use something else
-AUTHORIZE_CALLBACK = "http://that1song.com:5000/authorize"
+AUTHORIZE_CALLBACK = "http://that1song.com/authorize"
 
 # The number of songs to return in searches.
 MAX_SONGS = 32
