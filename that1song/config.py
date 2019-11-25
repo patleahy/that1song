c515eeb7b03d31562297ee3620e15adf78a461de
Pat Lehay <pat@patleahy.com>
2019-11-24 20:53:18 -0800

    Set session timeout less that spottily token timeout,

--- that1song/config.py
+++ that1song/config.py
@@ -5,8 +5,9 @@ SECRET_KEY              = environ.get("FLASK_SECRET_KEY")
 SPOTIFY_CLIENT_ID       = environ.get("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET   = environ.get("SPOTIFY_CLIENT_SECRET")
 
-SESSION_TYPE = 'filesystem' # TODO Use something else
-SESSION_TIMEOUT_MINUTES = 30 # Spotify tokens timeout in 60 min.
+SESSION_TYPE = 'filesystem'             # TODO Use something else
+PERMANENT_SESSION_LIFETIME = 30 * 60    # 30 minutes,  Spotify token timeout is 60 min
+
 AUTHORIZE_CALLBACK = "https://that1song.com/authorize"
 
 # The number of songs to return in searches.
